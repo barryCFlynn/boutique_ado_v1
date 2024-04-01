d56657ce0d12b6925ef23d33df594dca37974533
barry flynn <barry.c.flynn@gmail.com>
2024-04-01 11:39:26 +0000

    added sorting

--- products/views.py
+++ products/views.py
@@ -12,8 +12,25 @@ def all_products(request):
     # setup of the search using dajango Q to allow searching in name or description
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                # adds '-' to sort key to change it to decending
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -28,11 +45,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products' : products,
         'search_term' : query,
         'current_categoreis' : categories,
+        'current_sorting' : current_sorting,
     }
 
     return render(request, 'products/products.html', context)
