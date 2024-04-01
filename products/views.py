d4493562a65423e663dc718b9f7e222ba30c9389
barry flynn <barry.c.flynn@gmail.com>
2024-04-01 13:58:12 +0000

    added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -9,7 +9,6 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    # setup of the search using dajango Q to allow searching in name or description
     query = None
     categories = None
     sort = None
@@ -22,15 +21,14 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
-                # adds '-' to sort key to change it to decending
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -48,10 +46,10 @@ def all_products(request):
     current_sorting = f'{sort}_{direction}'
 
     context = {
-        'products' : products,
-        'search_term' : query,
-        'current_categoreis' : categories,
-        'current_sorting' : current_sorting,
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
