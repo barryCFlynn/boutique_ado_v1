41d7ebf8689aceb6d9338f017497cf34392346dc
barry flynn <barry.c.flynn@gmail.com>
2024-04-19 08:47:59 +0000

    added basic checkout functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +29,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-barrycflynn-boutiqueado-ylaum2uredj.ws-eu110.gitpod.io',]
 
 
 # Application definition
