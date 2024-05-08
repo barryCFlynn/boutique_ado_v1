4f851191b80da1e58e0f6df1b83cbbc57fb0b035
barry flynn <barry.c.flynn@gmail.com>
2024-05-08 10:51:42 +0000

    product admin - add product view

--- products/views.py
+++ products/views.py
@@ -75,7 +75,7 @@ def add_product(request):
     form = ProductForm()
     template = 'products/add_product.html'
     context = {
-        'form': form
+        'form': form,
     }
 
     return render(request, template, context)
